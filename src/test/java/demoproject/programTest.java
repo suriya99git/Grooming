cf3011b7f91530b385a039e47f105d23d0bf204a
suriya99git <suriyalenova99@gmail.com>
2024-02-12 23:25:21 +0530

    messsage

---
+++ src/test/java/demoproject/programTest.java
@@ -0,0 +1,11 @@
+package demoproject;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class programTest {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver = new ChromeDriver();
+	}
+}
